3e8e0f6b8bfd305fea60d72efdc7e88cea2cf4a3
kessystaub <kessystaub@outlook.com>
2023-05-27 17:34:42 -0300

    menu

--- src/Menu.js
+++ src/Menu.js
@@ -23,8 +23,8 @@ function Menu() {
   return (
     <div>
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <h4>Match Code</h4>
-      <div className='pl-1'>
+      <h4 className='m-2'>Match Code</h4>
+      <div className='pl-2'>
         <button className="navbar-toggler" type="button" data-toggle="collapse"
           data-target="#navbarSupportedContent"
           aria-controls="navbarSupportedContent" aria-expanded="false"
@@ -36,20 +36,20 @@ function Menu() {
           <ul className="navbar-nav mr-auto">
             <div className='m-1'>
               <li className="nav-item active">
-                <button className="btn btn-outline-primary" onClick={navigateToPerfil}>Perfil</button>
+                <button className="btn" onClick={navigateToPerfil}>Perfil</button>
               </li>
             </div>
             <div className='m-1'>
               <li className="nav-item">
-                <button className="btn btn-outline-primary" onClick={navigateToVaga}>Vagas</button>
+                <button className="btn" onClick={navigateToVaga}>Vagas</button>
               </li>
             </div>
           </ul>
           <div className='m-1'>
-            <button className="btn btn-outline-primary my-2 my-sm-0" onClick={navigateToCandidaturas} type="button">Candidaturas</button>
+            <button className="btn my-2 my-sm-0" onClick={navigateToCandidaturas} type="button">Candidaturas</button>
           </div>
           <div className='m-1'>
-            <button className="btn btn-outline-success my-2 my-sm-0" onClick={navigateToLogin} type="button">Sou Empresa</button>
+            <button className="btn my-2 my-sm-0" onClick={navigateToLogin} type="button">Sou Empresa</button>
           </div>
         </div>
       </div>
