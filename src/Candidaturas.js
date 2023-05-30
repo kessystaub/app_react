1c9e5ac9939cb7cafd96c8717f20e38030bf7697
kessystaub <kessystaub@outlook.com>
2023-05-30 20:45:30 -0300

    Fix

--- src/Candidaturas.js
+++ src/Candidaturas.js
@@ -6,9 +6,7 @@ function Candidaturas() {
   const navigate = useNavigate();
 
   const [applications, setApplications] = useState([]);
-  const [status, setStatus] = useState('');
-  const [jobName, setJobname] = useState('');
-  const [jobCode, setJobcode] = useState('');
+  const [user, setUser] = useState();
 
   function navigateToVaga(vaga_id) {
     localStorage.setItem("vaga_id", vaga_id)
@@ -21,54 +19,21 @@ function Candidaturas() {
 
   useEffect(() => {
     const storedValue = localStorage.getItem('user-info') ? JSON.parse(localStorage.getItem('user-info')) : [];
-    console.log(storedValue)
-    setApplications(storedValue.applications)
-  }, [setApplications]);
+    const user = storedValue.user;
+    setUser(user)
 
-  function returnStatusName(status_id) {
     const fetchData = async () => {
       try {
-        const response = await fetch(`http://localhost:8000/status/${status_id}`);
+        const response = await fetch(`http://localhost:8000/user/get_candidaturas_do_usuario/${user.id}`);
         const data = await response.json();
-        setStatus(data.result.status)
+        setApplications(data.result)
         } catch (error) {
           console.error('Error fetching data:', error);
         }
       };
 
     fetchData();
-    return status
-  }
-
-  function returnJobName(joboffer_id) {
-    const fetchData = async () => {
-      try {
-        const response = await fetch(`http://localhost:8000/joboffer/${joboffer_id}`);
-        const data = await response.json();
-        setJobname(data.result.name)
-        } catch (error) {
-          console.error('Error fetching data:', error);
-        }
-      };
-
-    fetchData();
-    return jobName
-  }
-
-  function returnJobCode(joboffer_id) {
-    const fetchData = async () => {
-      try {
-        const response = await fetch(`http://localhost:8000/joboffer/${joboffer_id}`);
-        const data = await response.json();
-        setJobcode(data.result.code)
-        } catch (error) {
-          console.error('Error fetching data:', error);
-        }
-      };
-
-    fetchData();
-    return jobCode
-  }
+  }, [setUser, applications]);
 
 
   return (
@@ -86,12 +51,12 @@ function Candidaturas() {
         <div className="form-inline justify-content-center">
 
         {applications.map((item) => (
-          <div className="card m-3" key={item.id}>
+          <div key={item.id} className="card m-3">
             <div className="card-body">
               <div className="form-inline">
-                <h5 className="card-title">{returnJobName(item.joboffer_id)}</h5>
+                <h5 className="card-title">{item.Joboffer.name}</h5>
               </div>
-              <p className="card-text">Situação: {returnStatusName(item.status_id)}</p>
+              <p className="card-text">Situação: {item.Status.status}</p>
               <div className="progress">
                 <div className="progress-bar progress-bar-striped
                   progress-bar-animated" role="progressbar" aria-valuenow="75"
@@ -99,9 +64,9 @@ function Candidaturas() {
               </div>
             </div>
             <ul className="list-group list-group-flush">
-              <li className="list-group-item">São Paulo, São Paulo, Brasil Remoto
+              <li className="list-group-item">Descrição: {item.Joboffer.description}
               </li>
-              <li className="list-group-item">Data da candidatura: {item.date}</li>
+              <li className="list-group-item">Data da candidatura: {item.Application.date}</li>
             </ul>
             <div className="card-body">
               <button className="btn btn-outline-success my-2 my-sm-0 m-3" onClick={() => navigateToVaga(item.joboffer_id)}>Descrição completa</button>
